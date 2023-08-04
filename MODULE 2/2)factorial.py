671b08bb52632c5d7344f1cc54845731d9eec7f0
Shivarsh <135205670+Shivarsh@users.noreply.github.com>
2023-08-04 12:21:30 +0530

    Add files via upload

---
+++ MODULE 2/2)factorial.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the Number :"))
+
+n=1
+for a in range(1,a+1):
+    n=n*a
+    
+print (n)
---
+++ MODULE 2/22)lessthantwocharacters.py
@@ -0,0 +1,7 @@
+mystr="shivarsh"
+
+if len(mystr)<2:
+    print([])
+else:
+    print(mystr[:2]+ mystr[-2:])
+
